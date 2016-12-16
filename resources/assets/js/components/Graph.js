a19b9f91bef9dbf46e473d339175fd8501520a2c
Chris Hughes <hughescp@aston.ac.uk>
2016-12-16 13:48:58 +0000

    Installed vue.js but encountered errors
    Installed vue.js and followed Charting and You: A Graph Vue Component.
    However run into errors, so will revert back to previous commit.

---
+++ resources/assets/js/components/Graph.js
@@ -0,0 +1,32 @@
+import Chart from 'chart.js';
+
+export default{
+    template: '<canvas width="600" height="400" id="graph"></canvas>',
+        
+    ready(){
+        
+        var data = {
+            labels: ['January', 'February', 'March'],
+
+            datasets: [
+                {
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,0.2)",
+                    pointColor: "rgba(220,220,220,0.2)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [30, 122, 90]
+                }
+            ]
+        };
+        
+        var context = document.querySelector('#graph').getContext('2d');
+
+        new Chart(context,{
+                type: "line",
+                data: data,
+        });
+
+    } 
+};
--- resources/views/areas/index.blade.php
+++ resources/views/areas/index.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="container">
-    <canvas width="200" height="200" id="graph"></canvas>
+    <graph></graph>
 </div>
 
 <script src="/js/main.js"></script>
@@ -19,7 +19,57 @@
                 <h2>{{$area->name}}</h2>
                 <h3>Overall Score:</h3>
                 <h3>7.1</h3>
-<!--
+                <table>
+                <tr>
+                    <td>
+                        <h4>Housing Affodability Ratio:</h4>
+                    </td>
+                    <td>
+                        <h4>{{$area->housing_affordability_ratio}}</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>Crime Level:</h4>
+                    </td>
+                    <td>
+                        <h4>{{$area->crime}}</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>Green Space:</h4>
+                    </td>
+                    <td>
+                        <h4>{{$area->greenspace*100}}%</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>5 Good GCSEs:</h4>
+                    </td>
+                    <td>
+                        <h4>{{$area->five_good_gcses*100}}%</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>Number of Pubs &amp; Restaraunts:</h4>
+                    </td>
+                    <td>
+                        <h4>{{$area->restaurants}}</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>Superfast Broadband:</h4>
+                    </td>
+                    <td>
+                        <h4>{{$area->superfast_broadband*100}}%</h4>
+                    </td>
+                </tr>
+            </table>
+                <!--
                 <canvas id="lineChart" height="300" width="300"></canvas>
                 <script type="text/javascript">
 
