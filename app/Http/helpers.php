26da29a52ff8388bd008b1b28c966bd1811e64dd
Chris Hughes <hughescp@aston.ac.uk>
2016-12-23 00:51:38 +0000

    Sucessfully implemented the function to calculate the overall score. However need to go look at algorithm as the results are different to excel.

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -3,14 +3,6 @@ use App\Area;
 
 class Helpers{
 
-//    define("HAR_WEIGHTING", 0.252809);
-//    define("CRIME_WEIGHTING", 0.191011);
-//    define("BROADBAND_WEIGHTING", 0.185393);
-//    define("GREENSPACE_WEIGHTING", 0.162921);
-//    define("GCSE_WEIGHTING", 0.134831);
-//    define("RESTAURANTS_WEIGHTING", 0.073034);
-
-
     public static function randomNumber()
     {
 
@@ -19,51 +11,130 @@ class Helpers{
     }
 
 
-    public static function getMaxValue($column_name){
-        $max_value = DB::select('SELECT MAX(:column_name) FROM areas', ['column_name' => $column_name]);
+//    public static function getMaxValue($column_name){
+//        print_r("#starting getMaxValue() function<br />");
+//
+//        $maxDB_values = DB::select('SELECT MAX(housing_affordability_ratio) AS max_value FROM areas');
+//
+////        $maxDB_values = DB::select('SELECT MAX(?) AS max_value FROM areas', [$column_name]); //Not name binding
+//
+//        //        $maxDB_values = DB::select('SELECT MAX(:column_name) AS max_value FROM areas', ['column_name' => $column_name]); //Name binding doesn't seem to work.
+//
+////        $maxDB_values = DB::select('SELECT MAX(:column_name) AS max_value FROM areas', ['column_name' => $column_name]);
+//
+////        $maxDB_values = DB::select('SELECT * FROM areas where name=(:column_name)', ['column_name' => 'Birmingham']);
+//
+//        print("#fetched max value from database<br />");
+//
+//        /*According to the documentation: The select method will always return an array of results. Each result within the array will be a PHP StdClass object, allowing you to access the values of the results:*/
+//
+//        print_r ($maxDB_values);
+//        print_r("<br />#var dump follows:<br />");
+//        var_dump($maxDB_values);
+//        print_r("<br />#var dump of specific value follows:<br />");
+//        print_r ($maxDB_values[0]);
+//        print_r("<br />#Housing Affordability Ratio:<br />");
+//        print_r ($maxDB_values[0]->max_value);
+//        //$max_value = $maxDB_values[0][0]->$column_name;
+//
+//        return $maxDB_values;
+//    }
 
-        return $max_value;
+//    SELECT * FROM areas WHERE name='Birmingham' returns the following
+/*    Array
+        ( [0] => stdClass Object
+         ( [id] => 1
+          [name] => Birmingham
+          [greenspace] => 0.48
+          [housing_affordability_ratio] => 4.3
+          [superfast_broadband] => 0.85
+          [crime] => 29.3
+          [five_good_gcses] => 0.88
+          [restaurants] => 6.9
+          [created_at] => 2016-12-13 11:35:37
+          [updated_at] => 2016-12-13 11:35:37 ) )
+    array(1) {
+        [0]=> object(stdClass)#152 (10)
+        { ["id"]=> int(1)
+            ["name"]=> string(10) "Birmingham"
+            ["greenspace"]=> float(0.48) ["housing_affordability_ratio"]=> float(4.3) ["superfast_broadband"]=> float(0.85)
+            ["crime"]=> float(29.3)
+            ["five_good_gcses"]=> float(0.88)
+            ["restaurants"]=> float(6.9)
+            ["created_at"]=> string(19) "2016-12-13 11:35:37" ["updated_at"]=> string(19) "2016-12-13 11:35:37"
+        }
     }
 
+//    SELECT MAX(:column_name) FROM areas' returns the following:
+        Array
+            ( [0] => stdClass Object
+            ( [MAX(?)] => housing_affordability_ratio ) )
+        array(1) {
+            [0]=> object(stdClass)#152 (1)
+            { ["MAX(?)"]=> string(27) "housing_affordability_ratio" } }
+
+*/
 
     public static function calculateOverallScore($area){
-
-        $har_weightings = 0.252809;
+        /*Weightings*/
+        $har_weighting = 0.252809;
         $crime_weighting = 0.191011;
         $broadband_weighting = 0.185393;
         $greenspace_weighting = 0.162921;
         $gcse_weighting = 0.134831;
         $restaurants_weighting = 0.073034;
 
+        /*Functions to fetch the maximum instance of each variable*/
+        #Housing Affordability Ratio
+        $maxDB_HAR_values = DB::select('SELECT MAX(housing_affordability_ratio) AS max_value FROM areas');
+        $har_max_value = $maxDB_HAR_values[0]->max_value;
+        #Crime
+        $maxDB_crime_values = DB::select('SELECT MAX(crime) AS max_value FROM areas');
+        $crime_max_value = $maxDB_crime_values[0]->max_value;
+        #Broadband
+        $maxDB_broadband_values = DB::select('SELECT MAX(superfast_broadband) AS max_value FROM areas');
+        $broadband_max_value = $maxDB_broadband_values[0]->max_value;
+        #Greenspace
+        $maxDB_greenspace_values = DB::select('SELECT MAX(greenspace) AS max_value FROM areas');
+        $greenspace_max_value = $maxDB_greenspace_values[0]->max_value;
+        #Good GCSEs
+        $maxDB_gcses_values = DB::select('SELECT MAX(five_good_gcses) AS max_value FROM areas');
+        $gcses_max_value = $maxDB_gcses_values[0]->max_value;
+        #Number of Pubs & Restaurants
+        $maxDB_restaurants_values = DB::select('SELECT MAX(restaurants) AS max_value FROM areas');
+        $restaurants_max_value = $maxDB_restaurants_values[0]->max_value;
+
         $areas = Area::all();
 
-        $harScore = $har_weightings*10;
+        /*Normalise the values:*/
+        $har_normalised = 1-($area->housing_affordability_ratio/$har_max_value);
 
-        $crimeScore = $crime_weighting*10;
+        $crime_normalised = 1-($area->crime/$crime_max_value);
 
-        $broadbandScore = $broadband_weighting*10;
+        $broadband_normalised = $area->superfast_broadband/$broadband_max_value;
 
-        $greenSpaceScore = $greenspace_weighting*10;
+        $greenspace_normalised = $area->greenspace/$greenspace_max_value;
 
-        $goodGCSEsScore = $gcse_weighting*10;
+        $gcses_normalised = $area->five_good_gcses/$gcses_max_value;
 
-        $restaurantsScore = $restaurants_weighting*10;
+        $restaurants_normalised = $area->restaurants/$restaurants_max_value;
 
-//
-//        $harScore = HAR_WEIGHTING*(self::getMaxValue('housing_affordability_ratio'));
-//
-//        $crimeScore = CRIME_WEIGHTING*(self::getMaxValue('crime'));
-//
-//        $broadbandScore = BROADBAND_WEIGHTING*(self::getMaxValue('superfast_broadband'));
-//
-//        $greenSpaceScore = GREENSPACE_WEIGHTING*(self::getMaxValue('greenspace'));
-//
-//        $goodGCSEsScore = GCSE_WEIGHTING*(self::getMaxValue('five_good_gcses'));
-//
-//        $restaurantsScore = RESTAURANTS_WEIGHTING*(self::getMaxValue('restaurants'));
+
+        $harScore = $har_normalised*$har_weighting;
+
+        $crimeScore = $crime_normalised*$crime_weighting;
+
+        $broadbandScore = $broadband_normalised*$broadband_weighting;
+
+        $greenSpaceScore = $greenspace_normalised*$greenspace_weighting;
+
+        $goodGCSEsScore = $gcses_normalised*$gcse_weighting;
+
+        $restaurantsScore = $restaurants_normalised*$restaurants_weighting;
 
 
         $overallScore = ($harScore + $crimeScore + $broadbandScore + $greenSpaceScore + $goodGCSEsScore + $restaurantsScore)*10;
+        $overallScore = number_format((float)$overallScore, 1, '.', ''); //this function rounds the number to 1 decimal place.
 
         return $overallScore;
     }
